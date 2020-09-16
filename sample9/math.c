6bdc212dfe8d1f66cf12bcb1ee27bd1d060d9f1a
Shinji-Sugasawa <68045030+Shinji-Sugasawa@users.noreply.github.com>
2020-09-16 22:34:47 +0900

    Create math.c

---
+++ sample9/math.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int dev(int a, int b);
+int mod(int a, int b);
+
+int main(int argc,char *argv[]){
+
+	int a,b,ans;
+
+	printf("a =");
+	scanf("%d", &a);
+        printf("b =");
+	scanf("%d", &b);
+
+	ans = add(a, b);
+	printf("add = %d\n",ans);
+        ans = sub(a, b);
+        printf("sub = %d\n",ans);
+        ans = mul(a, b);
+        printf("mul = %d\n",ans);
+        ans = dev(a, b);
+        printf("dev = %d\n",ans);
+        ans = mod(a, b);
+        printf("mod = %d\n",ans);
+	return 0;
+}
